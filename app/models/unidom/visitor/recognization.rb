47493437b0e9cfe2c5521f651e6d4c458c74d2eb
Topbit Du <topbit.du@gmail.com>
2016-08-30 10:24:13 +0800

    1, Improved the Recognization model to add the ::cognize! method. 2, Improved the Recognization model to deprecate the ::cognize method.

--- app/models/unidom/visitor/recognization.rb
+++ app/models/unidom/visitor/recognization.rb
@@ -13,9 +13,26 @@ class Unidom::Visitor::Recognization < ActiveRecord::Base
   include Unidom::Common::Concerns::ModelExtension
 
   def self.cognize(visitor, party, elemental: true, opened_at: Time.now)
+    warn "The Unidom::Visitor::Recognization.cognize method is deprecated, and will be removed since unidom-visitor v1.0. Please use the Unidom::Visitor::Recognization.cognize method instead."
     raise 'Visitor can not be null.' if visitor.blank?
     raise 'Party can not be null.'   if party.blank?
     recognization = visitor_is(visitor).party_is(party).first_or_create elemental: elemental, opened_at: opened_at
+  end
+
+  def self.cognize!(visitor, as: nil, at: Time.now, primary: false)
+    query = visitor_is(visitor).party_is(as).valid_at.alive
+    recognization = query.first
+    if recognization.present?
+      recognization.elemental = primary
+      recognization.save!
+      recognization
+    else
+      query.create! elemental: primary, opened_at: at
+    end
+  end
+
+  class << self
+    deprecate cognize: :cognize!, deprecator: ActiveSupport::Deprecation.new('1.0', 'unidom-visitor')
   end
 
 end
