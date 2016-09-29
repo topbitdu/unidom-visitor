e2a69ecf8f5a3595b111f3d6970a37c30052a68e
Topbit Du <topbit.du@gmail.com>
2016-09-29 09:52:28 +0800

    1, Improve the User model to include the As Visitor concern.

--- app/models/unidom/visitor/user.rb
+++ app/models/unidom/visitor/user.rb
@@ -4,16 +4,19 @@ class Unidom::Visitor::User < ActiveRecord::Base
 
   self.table_name = 'unidom_users'
 
-  has_many :identificatings, class_name: 'Unidom::Visitor::Identificating', as: :visitor
+  #has_many :identificatings, class_name: 'Unidom::Visitor::Identificating', as: :visitor
 
-  has_many :authenticatings, class_name: 'Unidom::Visitor::Authenticating', as: :visitor
-  has_many :passwords,       through:    :authenticatings, source: :credential, source_type: 'Unidom::Visitor::Password'
+  #has_many :authenticatings, class_name: 'Unidom::Visitor::Authenticating', as: :visitor
+  #has_many :passwords,       through:    :authenticatings, source: :credential, source_type: 'Unidom::Visitor::Password'
 
-  has_many :recognizations,  class_name: 'Unidom::Visitor::Recognization',  as: :visitor
+  #has_many :recognizations,  class_name: 'Unidom::Visitor::Recognization',  as: :visitor
 
-  scope :identified_by, ->(identity) { joins(:identificatings).merge(Unidom::Visitor::Identificating.identity_is identity) }
+  #scope :identified_by, ->(identity) { joins(:identificatings).merge(Unidom::Visitor::Identificating.identity_is identity) }
 
   include Unidom::Common::Concerns::ModelExtension
+  include Unidom::Visitor::Concerns::AsVisitor
+
+  has_many :passwords, through: :authenticatings, source: :credential, source_type: 'Unidom::Visitor::Password'
 
   def self.sign_up(identity, password: nil, opened_at: Time.now)
 
