cde677d9315c412fa0dee79b9de1172e1c0d5f57
Topbit Du <topbit.du@gmail.com>
2016-10-03 15:48:39 +0800

    1, Improve the Guest model for the coding style.

--- app/models/unidom/visitor/guest.rb
+++ app/models/unidom/visitor/guest.rb
@@ -4,10 +4,10 @@ class Unidom::Visitor::Guest < ActiveRecord::Base
 
   self.table_name = 'unidom_guests'
 
+  include Unidom::Common::Concerns::ModelExtension
+
   validates :platform_specific_identification, presence: true, length: { in: 2..self.columns_hash['platform_specific_identification'].limit }
 
   scope :platform_specific_identification_is, ->(platform_specific_identification) { where platform_specific_identification: platform_specific_identification }
-
-  include Unidom::Common::Concerns::ModelExtension
 
 end
