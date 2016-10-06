c45549b345f24a6cf884c1d020e291044edc7126
Topbit Du <topbit.du@gmail.com>
2016-10-07 01:18:41 +0800

    1, Improve the Identificating model for the coding style.

--- app/models/unidom/visitor/identificating.rb
+++ app/models/unidom/visitor/identificating.rb
@@ -4,13 +4,13 @@ class Unidom::Visitor::Identificating < ActiveRecord::Base
 
   self.table_name = 'unidom_identificatings'
 
+  include Unidom::Common::Concerns::ModelExtension
+
   belongs_to :identity, polymorphic: true
   belongs_to :visitor,  polymorphic: true
 
   scope :identity_is, ->(identity) { where identity: identity }
   scope :visitor_is,  ->(visitor)  { where visitor:  visitor  }
-
-  include Unidom::Common::Concerns::ModelExtension
 
   def self.find_identity(visitor)
     visitor_is(visitor).first.try :identity
