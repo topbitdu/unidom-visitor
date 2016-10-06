db3cc2062101e958578e5f542027a6c2a5bbd3a9
Topbit Du <topbit.du@gmail.com>
2016-10-07 02:27:59 +0800

    1, Improve the Recognition model for the coding style.

--- app/models/unidom/visitor/recognization.rb
+++ app/models/unidom/visitor/recognization.rb
@@ -4,13 +4,13 @@ class Unidom::Visitor::Recognization < ActiveRecord::Base
 
   self.table_name = 'unidom_recognizations'
 
+  include Unidom::Common::Concerns::ModelExtension
+
   belongs_to :visitor, polymorphic: true
   belongs_to :party,   polymorphic: true
 
   scope :visitor_is,  ->(visitor) { where visitor: visitor }
   scope :party_is,    ->(party)   { where party:   party   }
-
-  include Unidom::Common::Concerns::ModelExtension
 
   def self.cognize!(visitor, as: nil, at: Time.now, primary: false)
     query = visitor_is(visitor).party_is(as).valid_at.alive
