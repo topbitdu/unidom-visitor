cb104dfeb330825310befa485db381d097822907
Topbit Du <topbit.du@gmail.com>
2016-10-11 07:21:29 +0800

    1, Improve the As Credential concern to add the #authenticate! method.

--- app/models/unidom/visitor/concerns/as_credential.rb
+++ app/models/unidom/visitor/concerns/as_credential.rb
@@ -6,10 +6,10 @@ module Unidom::Visitor::Concerns::AsCredential
 
     has_one :authenticating, class_name: 'Unidom::Visitor::Authenticating', as: :credential
 
-    #def authenticate!(it, at: Time.now, flag_code: 'RQRD')
-    #  return authenticating if authenticating.present?
-    #  create_authenticating! visitor: it, flag_code: flag_code, opened_at: at
-    #end
+    def authenticate!(it, at: Time.now, flag_code: 'RQRD')
+      return authenticating if authenticating.present?
+      create_authenticating! visitor: it, flag_code: flag_code, opened_at: at
+    end
 
   end
 
