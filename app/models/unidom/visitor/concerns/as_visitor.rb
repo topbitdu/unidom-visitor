2d0f9b1abc34b991d8c5e8321aba3c0ef831c0b6
Topbit Du <topbit.du@gmail.com>
2016-10-11 10:07:16 +0800

    1, Improve the As Visitor concern to add the #is_authenticated! method.

--- app/models/unidom/visitor/concerns/as_visitor.rb
+++ app/models/unidom/visitor/concerns/as_visitor.rb
@@ -16,9 +16,33 @@ module Unidom::Visitor::Concerns::AsVisitor
       identificatings.create! identity: as, opened_at: at
     end
 
+    def is_authenticated!(through: nil, at: Time.now, flag_code: 'RQRD')
+      if through.authenticating.present?
+        through.authenticating
+      else
+        authenticatings.create! credential: through, opened_at: at, flag_code: flag_code
+      end
+    end
+
+=begin
+    def cognize!(it, at: Time.now, primary: true)
+      recognizations.create! party: it, elemental: primary, opened_at: at
+    end
+=end
+
   end
 
   module ClassMethods
+
+=begin
+    def sign_up!(it, as: nil, through: nil, at: Time.now, flag_code: 'RQRD', primary: true)
+      cognize! it,      primary: true,    at: at
+      is_identificated! as:      as,      at: at
+      is_authenticated! through: through, at: at
+      self
+    end
+=end
+
   end
 
 end
