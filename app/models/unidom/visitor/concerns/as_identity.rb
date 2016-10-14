d8caa3de633697514bbe28b53f0efb01e4779e69
Topbit Du <topbit.du@gmail.com>
2016-10-14 10:45:32 +0800

    1, Improve the As Identity concern to add the #identificate? method.

--- app/models/unidom/visitor/concerns/as_identity.rb
+++ app/models/unidom/visitor/concerns/as_identity.rb
@@ -10,6 +10,13 @@ module Unidom::Visitor::Concerns::AsIdentity
       identificatings.create! visitor: it, opened_at: at
     end
 
+    def identificate?(it, at: Time.now)
+      query = identificatings
+      query = query.visitor_is it      if it.present?
+      query = query.valid_at   now: at if at.present?
+      query.alive.exists?
+    end
+
   end
 
   module ClassMethods
