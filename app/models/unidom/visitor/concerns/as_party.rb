692ccc390904853a6f0f5ba657fa0cdd4e1b74d5
Topbit Du <topbit.du@gmail.com>
2016-10-16 14:26:41 +0800

    1, Improve the As Party concern to add the #is_cognized? method.

--- app/models/unidom/visitor/concerns/as_party.rb
+++ app/models/unidom/visitor/concerns/as_party.rb
@@ -10,6 +10,14 @@ module Unidom::Visitor::Concerns::AsParty
       recognizations.create! visitor: via, elemental: primary, opened_at: at
     end
 
+    def is_cognized?(via: nil, at: Time.now, primary: true)
+      query = recognizations
+      query = query.visitor_is via     if     via.present?
+      query = query.primary    primary unless primary.nil?
+      query = query.valid_at   now: at if     at.present?
+      query = query.alive.exists?
+    end
+
   end
 
   module ClassMethods
