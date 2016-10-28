7b4a3e4048254d43e6f95804a59a2c366800765b
Topbit Du <topbit.du@gmail.com>
2016-10-28 10:50:22 +0800

    1, Improve the Identificating model for Rails 5.

--- app/models/unidom/visitor/identificating.rb
+++ app/models/unidom/visitor/identificating.rb
@@ -1,6 +1,6 @@
 # Identificating 是身份关联，存储身份(identity)和访问者(visitor)之间的关联关系。
 
-class Unidom::Visitor::Identificating < ActiveRecord::Base
+class Unidom::Visitor::Identificating < Unidom::Visitor::ApplicationRecord
 
   self.table_name = 'unidom_identificatings'
 
