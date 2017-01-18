14bd1f8d715eb6865e88d3d40d4ac7c10311d0d8
Topbit Du <topbit.du@gmail.com>
2017-01-18 08:53:46 +0800

    1, Improve the Recognization model for the document.

--- app/models/unidom/visitor/recognization.rb
+++ app/models/unidom/visitor/recognization.rb
@@ -13,6 +13,9 @@ class Unidom::Visitor::Recognization < Unidom::Visitor::ApplicationRecord
   scope :visitor_is,  ->(visitor) { where visitor: visitor }
   scope :party_is,    ->(party)   { where party:   party   }
 
+  ##
+  # 将访问者 visitor 和参与者 party 关联起来。关联时间为 at ，缺省为当前时间。主关联标志为 primary ，缺省为 false 。如：
+  # Unidom::Visitor::Recognization.cognize! user, as: selected_person
   def self.cognize!(visitor, as: nil, at: Time.now, primary: false)
     query = visitor_is(visitor).party_is(as).valid_at.alive
     recognization = query.first
