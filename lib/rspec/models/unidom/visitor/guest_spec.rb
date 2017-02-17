ff20efa344451ae5c9a3b7622854f20f4fd6f637
Topbit Du <topbit.du@gmail.com>
2017-02-17 10:15:38 +0800

    1, Improve the Guest spec for the validations on the #platform_specific_identification attribute.

--- lib/rspec/models/unidom/visitor/guest_spec.rb
+++ lib/rspec/models/unidom/visitor/guest_spec.rb
@@ -13,7 +13,26 @@ describe Unidom::Visitor::Guest, type: :model do
       platform_specific_identification: '123456789012'
     }
 
+    platform_specific_identification_max_length = described_class.columns_hash['platform_specific_identification'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :platform_specific_identification,
+      {                                         } => 0,
+      { platform_specific_identification: nil   } => 2,
+      { platform_specific_identification: ''    } => 2,
+      { platform_specific_identification: '1'   } => 1,
+      { platform_specific_identification: 'A'   } => 1,
+      { platform_specific_identification: '11'  } => 0,
+      { platform_specific_identification: 'AA'  } => 0,
+      { platform_specific_identification: '111' } => 0,
+      { platform_specific_identification: 'AAA' } => 0,
+      { platform_specific_identification: '1'*(platform_specific_identification_max_length-1) } => 0,
+      { platform_specific_identification: 'A'*(platform_specific_identification_max_length-1) } => 0,
+      { platform_specific_identification: '1'*platform_specific_identification_max_length     } => 0,
+      { platform_specific_identification: 'A'*platform_specific_identification_max_length     } => 0,
+      { platform_specific_identification: '1'*(platform_specific_identification_max_length+1) } => 1,
+      { platform_specific_identification: 'A'*(platform_specific_identification_max_length+1) } => 1
 
   end
 
