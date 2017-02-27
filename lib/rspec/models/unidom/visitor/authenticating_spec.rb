070ab7818211e3046678bbf7436843540924442e
Topbit Du <topbit.du@gmail.com>
2017-02-27 18:16:48 +0800

    1, Improve the Authenticating spec for the ``belongs_to :visitor, polymorphic: true`` macro, & the ``belongs_to :credential, polymorphic: true`` macro.

--- lib/rspec/models/unidom/visitor/authenticating_spec.rb
+++ lib/rspec/models/unidom/visitor/authenticating_spec.rb
@@ -20,6 +20,14 @@ describe Unidom::Visitor::Authenticating, type: :model do
 
     it_behaves_like 'ProgneTapera::EnumCode', described_class.new(model_attributes), :flag, Unidom::Visitor::Flag
 
+    user_attributes     = {}
+    guest_attributes    = { platform_code: 'SITE', platform_specific_identification: '123456789012' }
+    password_attributes = { clear_text: 'password' }
+
+    it_behaves_like 'belongs_to', model_attributes, :visitor,    Unidom::Visitor::User,     user_attributes
+    it_behaves_like 'belongs_to', model_attributes, :visitor,    Unidom::Visitor::Guest,    guest_attributes
+    it_behaves_like 'belongs_to', model_attributes, :credential, Unidom::Visitor::Password, password_attributes
+
   end
 
 end
