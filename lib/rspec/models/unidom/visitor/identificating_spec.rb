e663e4f59911c8b4dd74da3c2269910611e96e32
Topbit Du <topbit.du@gmail.com>
2017-03-06 22:23:33 +0800

    1, Improve the Identificating spec for the visitor_is scope.

--- lib/rspec/models/unidom/visitor/identificating_spec.rb
+++ lib/rspec/models/unidom/visitor/identificating_spec.rb
@@ -17,11 +17,13 @@ describe Unidom::Visitor::Identificating, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
-    user_attributes     = {}
-    guest_attributes    = { platform_code: 'SITE', platform_specific_identification: '123456789012' }
+    user_attributes  = {}
+    guest_attributes = { platform_code: 'SITE', platform_specific_identification: '123456789012' }
 
     it_behaves_like 'belongs_to', model_attributes, :visitor, Unidom::Visitor::User,  user_attributes
     it_behaves_like 'belongs_to', model_attributes, :visitor, Unidom::Visitor::Guest, guest_attributes
+
+    it_behaves_like 'polymorphic scope', model_attributes, :visitor_is, :visitor, [ Unidom::Visitor::User, Unidom::Visitor::Guest ]
 
   end
 
