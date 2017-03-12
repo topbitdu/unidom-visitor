952a85700f89981d187272a1c2dda5a92bf180a5
Topbit Du <topbit.du@gmail.com>
2017-03-12 09:17:07 +0800

    1, Improve the Recognization spec for the ``belongs_to :visitor, polymorphic: true`` macro.

--- lib/rspec/models/unidom/visitor/recognization_spec.rb
+++ lib/rspec/models/unidom/visitor/recognization_spec.rb
@@ -18,6 +18,12 @@ describe Unidom::Visitor::Recognization, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    user_attributes  = {}
+    guest_attributes = { platform_code: 'SITE', platform_specific_identification: '123456789012' }
+
+    it_behaves_like 'belongs_to', model_attributes, :visitor, Unidom::Visitor::User,  user_attributes
+    it_behaves_like 'belongs_to', model_attributes, :visitor, Unidom::Visitor::Guest, guest_attributes
+
   end
 
 end
