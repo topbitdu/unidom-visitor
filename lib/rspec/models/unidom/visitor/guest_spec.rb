f747728358d0b60323f17cfee5a44f0ae3705b6b
Topbit Du <topbit.du@gmail.com>
2017-03-18 16:06:14 +0800

    1, Improve the Guest spec for the validations on the #platform_specific_identification attribute.

--- lib/rspec/models/unidom/visitor/guest_spec.rb
+++ lib/rspec/models/unidom/visitor/guest_spec.rb
@@ -13,26 +13,10 @@ describe Unidom::Visitor::Guest, type: :model do
       platform_specific_identification: '123456789012'
     }
 
-    platform_specific_identification_max_length = described_class.columns_hash['platform_specific_identification'].limit
-
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
-    it_behaves_like 'validates', model_attributes, :platform_specific_identification,
-      {                                         } => 0,
-      { platform_specific_identification: nil   } => 2,
-      { platform_specific_identification: ''    } => 2,
-      { platform_specific_identification: '1'   } => 1,
-      { platform_specific_identification: 'A'   } => 1,
-      { platform_specific_identification: '11'  } => 0,
-      { platform_specific_identification: 'AA'  } => 0,
-      { platform_specific_identification: '111' } => 0,
-      { platform_specific_identification: 'AAA' } => 0,
-      { platform_specific_identification: '1'*(platform_specific_identification_max_length-1) } => 0,
-      { platform_specific_identification: 'A'*(platform_specific_identification_max_length-1) } => 0,
-      { platform_specific_identification: '1'*platform_specific_identification_max_length     } => 0,
-      { platform_specific_identification: 'A'*platform_specific_identification_max_length     } => 0,
-      { platform_specific_identification: '1'*(platform_specific_identification_max_length+1) } => 1,
-      { platform_specific_identification: 'A'*(platform_specific_identification_max_length+1) } => 1
+    it_behaves_like 'validates text', model_attributes, :platform_specific_identification,
+      length: 2..described_class.columns_hash['platform_specific_identification'].limit
 
     new_platform_specific_identification = 'ABCDEFGHIJKLMNOP'
     it_behaves_like 'scope', :platform_specific_identification_is, [
