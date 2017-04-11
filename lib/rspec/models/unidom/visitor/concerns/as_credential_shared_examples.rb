826c91e0f4bf78dffad603895146e59c4b2395d0
Topbit Du <topbit.du@gmail.com>
2017-04-11 10:08:47 +0800

    1, Improve the As Credential shared examples for the argument validations of the #authenticate! method.

--- lib/rspec/models/unidom/visitor/concerns/as_credential_shared_examples.rb
+++ lib/rspec/models/unidom/visitor/concerns/as_credential_shared_examples.rb
@@ -10,6 +10,10 @@ shared_examples 'Unidom::Visitor::Concerns::AsCredential' do |model_attributes|
 
     it_behaves_like 'has_one', model_attributes, :authenticating, Unidom::Visitor::Authenticating, authenticating_attributes
 
+    model   = described_class.create! model_attributes
+    visitor = Unidom::Visitor::User.create!
+    it_behaves_like 'assert_present!', model, :authenticate!, [ visitor, at: Time.now, flag_code: 'RQRD' ], [ { 0 => :it }, :at, :flag_code ]
+
   end
 
 end
