864f27080555e117568e95ec06a0d39c24ff465b
Topbit Du <topbit.du@gmail.com>
2017-04-21 17:15:39 +0800

    1, Improve the As Identity concern for the argument validations of the #identificate! method.

--- app/models/unidom/visitor/concerns/as_identity.rb
+++ app/models/unidom/visitor/concerns/as_identity.rb
@@ -1,13 +1,18 @@
 module Unidom::Visitor::Concerns::AsIdentity
 
-  extend ActiveSupport::Concern
+  extend  ActiveSupport::Concern
+  include Unidom::Common::Concerns::ArgumentValidation
 
   included do |includer|
 
     has_many :identificatings, class_name: 'Unidom::Visitor::Identificating', as: :identity
 
     def identificate!(it, at: Time.now)
+
+      assert_present! :it, it
+
       identificatings.create! visitor: it, opened_at: at
+
     end
 
     def identificate?(it, at: Time.now)
